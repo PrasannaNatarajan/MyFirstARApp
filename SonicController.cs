f3305b070b03cac13968bb7cc42d3b5e220a1b82
PrasannaNatarajan <prasanna2431997@gmail.com>
2017-06-23 12:11:49 +0530

    all scripts and fbx file

---
+++ SonicController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using Vuforia;
+
+
+public class SonicController : MonoBehaviour {
+
+	// Use this for initialization
+    private Rigidbody rb;
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = CrossPlatformInputManager.GetAxis("Horizontal");
+        float y = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, y);
+
+        rb.velocity = movement * 4f;
+
+        if (x != 0 && y != 0)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x,Mathf.Atan2(x,y) * Mathf.Rad2Deg ,transform.eulerAngles.z);
+        }
+
+        
+		
+	}
+}
---
+++ focusCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class focusCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        bool f = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        if (!f)
+        {
+            Debug.Log("Failed to set focus mode to continusauto (unsupported mode).");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
